fb82bc87d4f4403914d37aa87cddb2f692893f46
Mark Smith <smith.h.mark@gmail.com>
2019-04-05 16:07:25 -0700

    sim can deal with course changes

--- joust-ui/src/lib/sim.js
+++ joust-ui/src/lib/sim.js
@@ -49,7 +49,7 @@ const VesselClassType = shape({
 const VesselType = shape({
   pos: PositionType,
   vel: VelocityType,
-  maneuver: ManeuverOrderType,
+  maneuverOrder: ManeuverOrderType,
   cls: VesselClassType,
 });
 
@@ -77,15 +77,18 @@ export const nextWorld = (world, timeSinceLast) => {
     //noiseLevel = testForNoise(vessel);
     let newVes;
     
-    if (!vessel.maneuver) {
-      // displace vessel by its velocity
-      //console.log("displace vessels on legs");
-      //console.log("old pos", vessel.pos);
-      //console.log("vel", vessel.vel);
+    if (!vessel.maneuverOrder) { //vessel is on a leg
       let newPos = geom.posAfter(vessel.pos, vessel.vel, timeSinceLast);
-      //console.log("displaced to", newPos);
-      newVes = {vessel, pos: newPos};
+      newVes = {...vessel, pos: newPos};
     } else {
+      if (!vessel.maneuverComp) { // first we've seen of this order
+        // ERROR here when order is changed
+        console.log("calc accel");
+        
+      } else {
+        //continue with existing order
+        console.log("calc accel");
+      }
       // calc actual velocity from manuver. (Manu is like an accelleration)
       // displace vessel by its velocity
       console.log("calc accel");
@@ -98,4 +101,66 @@ export const nextWorld = (world, timeSinceLast) => {
     nextWorld.vessels.push(newVes);
   }
   return nextWorld;
+};
+
+export const turningCircleCenter = (pos, heading, radius, rudderDir) => {
+  let bearingToCenter;
+  if (rudderDir === "RIGHT") {
+    bearingToCenter = geom.bearingAdd(heading, 90);
+  } else {
+    bearingToCenter = geom.bearingMinus(heading, 90);
+  }
+  let fakeVel = geom.mkVel(bearingToCenter, radius);
+  return geom.posAfter(pos, fakeVel, 1);
+};
+
+export const nextPosInTurn = (
+  oldPos, oldVel,
+  radius, rudderDir, timeSinceLast
+) => {
+  let oldCS = geom.mkCrsSpd(oldVel);
+  let center = turningCircleCenter(oldPos, oldCS.crs, radius, rudderDir);
+
+  let dist = oldCS.spd * timeSinceLast;
+  //console.log("dist:", dist);
+  let circum = 2 * Math.PI * radius;
+  //console.log("circum:", circum);
+  let angleBetween = dist * 360 / circum;
+  let bToOld = geom.angleBetween(oldPos, center);
+  console.log("b from center to old pos:", bToOld);
+  let bFmCenToNew;
+  let nextCrs;
+  console.log("angleBetween:", angleBetween);
+
+  bFmCenToNew = geom.bearingAdd(bToOld, angleBetween);
+  nextCrs = geom.bearingAdd(bFmCenToNew, 90);
+  console.log("old cs", oldCS);
+  console.log("next C", nextCrs);
+  console.log("bearing offset:", geom.angularDiff(oldCS.crs, nextCrs)); 
+  if (angleBetween < 180 && geom.angularDiff(oldCS.crs, nextCrs) > 90 ){
+    bFmCenToNew = geom.bearingMinus(bToOld, angleBetween);
+    nextCrs = geom.bearingMinus(bFmCenToNew, 90);
+    console.log("correcting for rotating the wrong way");
+  }
+  console.log("b from center to new:", bFmCenToNew);
+  /*
+  if (rudderDir == "RIGHT") {
+    bFmCenToNew = geom.bearingAdd(bToOld, angleBetween);
+    nextCrs = geom.bearingAdd(bFmCenToNew, 90);
+    console.log("bFmCenToNew (adding):", bFmCenToNew);
+  } else {
+    bFmCenToNew = geom.bearingMinus(bToOld, angleBetween);
+    nextCrs = geom.bearingMinus(bFmCenToNew, 90);
+    console.log("bFmCenToNew (subtracting):", bFmCenToNew);
+  } 
+  */
+  let fakeVel = geom.mkVel(bFmCenToNew, radius);
+  //console.log("fakevel:", fakeVel);
+  console.log("center:", center);
+  let nextPos = geom.posAfter(center, fakeVel, 1);
+  console.log("nextPos:", nextPos);
+  let nextVel = geom.mkVel(nextCrs, oldCS.spd);
+  //console.log("nextVel:", nextVel);
+  //console.log("nextcs:", geom.mkCrsSpd(nextVel));
+  return {pos: nextPos, vel: nextVel, crs: nextCrs}
 };
--- joust-ui/src/lib/sim.test.js
+++ joust-ui/src/lib/sim.test.js
@@ -2,19 +2,53 @@
 import {
   mkPos,
   mkVel,
+  dispMagnatude,
+  displacement,
 } from './geom';
 
 import {
   newWorld,
   addVessel,
   nextWorld,
+  turningCircleCenter,
+  nextPosInTurn,
 } from './sim';
+
+expect.extend({
+  toMatchCloseTo(received, expected, margin=0.000001) {
+    for (let kk in expected) {
+      if (!received.hasOwnProperty(kk)) {
+        //console.log("missing prop:",kk);
+        return {
+          message: () =>
+            `expected ${JSON.stringify(received)} to have attribute: ${kk}`,
+          pass: false,
+        };
+      } else {
+        let err = Math.abs(expected[kk] - received[kk]);
+        if (err > margin) {
+          //console.log("has prop but off:",kk, err);
+          return {
+            message: () =>
+              `expected attribute ${kk} ${received[kk]} to be within ${margin} of ${expected[kk]}`,
+            pass: false,
+          };
+        }
+      }
+    }
+    return {
+      message: () =>
+        `expected all ${received} to be close to ${expected}`,
+      pass: true,
+    };
+  },
+});
 
 const buildTestVessel = (x, y, z, c, s, man=null, cls=null) => {
   return {
   pos: mkPos(x,y),
   vel: mkVel(c,s),
-  maneuver: man,
+  maneuverOrder: man,
   cls: cls,
   };
 };
@@ -57,6 +91,99 @@ describe("updating the world", () => {
     expect(updatedWorld).toHaveProperty("vessels");
     expect(updatedWorld).toHaveProperty("classes");
     expect(updatedWorld.vessels.length).toBe(1);
+    expect(updatedWorld.vessels[0]).toHaveProperty("pos");
+    expect(updatedWorld.vessels[0]).toHaveProperty("vel");
+    expect(updatedWorld.vessels[0]).toHaveProperty("maneuverOrder");
+    expect(updatedWorld.vessels[0]).toHaveProperty("cls");
     expect(updatedWorld.vessels[0].pos).toEqual(mkPos(5,0));
+  });
+});
+
+describe('interpreting orders', () => {
+  describe('interpreting course change orders', () => {
+    it('can find the center of a right turning circle', () => {
+      let oldPos = mkPos(0,5);
+      let r = 5, rudderDir = "RIGHT";
+      let oldheading = 0;
+      expect(turningCircleCenter(oldPos,oldheading,r,rudderDir))
+        .toMatchCloseTo(mkPos(5,5));
+      oldheading = 180;
+      expect(turningCircleCenter(oldPos,oldheading,r,rudderDir))
+        .toMatchCloseTo(mkPos(-5,5));
+    });
+    it('can find the center of a left turning circle', () => {
+      let oldPos = mkPos(0,5);
+      let r = 5, rudderDir = "LEFT";
+      let oldheading = 0;
+      expect(turningCircleCenter(oldPos,oldheading,r,rudderDir))
+        .toMatchCloseTo(mkPos(-5,5));
+      oldheading = 180;
+      expect(turningCircleCenter(oldPos,oldheading,r,rudderDir))
+        .toMatchCloseTo(mkPos(5,5));
+    });
+
+    describe("northbound courses", () =>{
+      it('can find the new pos along northbound RIGHT turning circle', () => {
+        let oldPos = mkPos(0,5);
+        let crs = 0, spd = 5;
+        let vel = mkVel(0, spd);
+        let dur = 1;
+        let r = 5, rudderDir = "RIGHT";
+        let nextVessel = nextPosInTurn(oldPos, vel, r, rudderDir, dur);
+        let expectedPos = mkPos(2.298,9.20);
+        let expectedVel = mkVel(57.2957,spd);
+        expect(nextVessel).toHaveProperty("pos");
+        expect(nextVessel).toHaveProperty("vel");
+        expect(nextVessel.pos).toMatchCloseTo(expectedPos, 0.1);
+        expect(nextVessel.vel).toMatchCloseTo(expectedVel, 0.001);
+      });
+      it('can find the new pos along northbound left turning circle', () => {
+        let oldPos = mkPos(0,5);
+        let crs = 0, spd = 5;
+        let vel = mkVel(0, spd);
+        let dur = 1;
+        let r = 5, rudderDir = "LEFT";
+        let nextVessel = nextPosInTurn(oldPos, vel, r, rudderDir, dur);
+        let expectedPos = mkPos(-2.298,9.20);
+        let expectedVel = mkVel(360 - 57.2857,spd);
+        expect(nextVessel).toHaveProperty("pos");
+        expect(nextVessel).toHaveProperty("vel");
+        expect(nextVessel.pos).toMatchCloseTo(expectedPos, 0.1);
+        expect(nextVessel.vel).toMatchCloseTo(expectedVel, 0.001);
+      });
+    });
+
+    describe("Southbound courses", () =>{
+      it('can find the new pos along southbound RIGHT turning circle', () => {
+        let oldPos = mkPos(0,5);
+        let crs = 180, spd = 5;
+        let vel = mkVel(crs, spd);
+        let dur = 1;
+        let r = 5, rudderDir = "RIGHT";
+        let nextVessel = nextPosInTurn(oldPos, vel, r, rudderDir, dur);
+        let expectedPos = mkPos(-2.298, 5-4.20);
+        let expectedVel = mkVel(180 + 57.2957,spd);
+        expect(nextVessel).toHaveProperty("pos");
+        expect(nextVessel).toHaveProperty("vel");
+        expect(nextVessel.pos).toMatchCloseTo(expectedPos, 0.1);
+        expect(nextVessel.vel).toMatchCloseTo(expectedVel, 0.01);
+      });
+
+      it('can find the new pos along southbound left turning circle', () => {
+        let oldPos = mkPos(0,5);
+        let crs = 180, spd = 5;
+        let vel = mkVel(crs, spd);
+        let dur = 1;
+        let r = 5, rudderDir = "LEFT";
+        let nextVessel = nextPosInTurn(oldPos, vel, r, rudderDir, dur);
+        let expectedPos = mkPos(2.298,5 - 4.20);
+        let expectedVel = mkVel(180 - 57.2857,spd);
+        expect(nextVessel).toHaveProperty("pos");
+        expect(nextVessel).toHaveProperty("vel");
+        expect(nextVessel.pos).toMatchCloseTo(expectedPos, 0.01);
+        expect(nextVessel.vel).toMatchCloseTo(expectedVel, 0.001);
+      });
+    });
+
   });
 });
